555981251dac807455bf2a3b7f5450916a71f4ac
Hari Charan Kandregula <haricharanwork@gmail.com>
2023-10-01 07:40:56 +0530

    login

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import HeroSection from "../components/common/HeroSection";
 import Header from "../components/common/Header";
 import ProductShowcase from "../components/common/ProductShowcase/productshowcase";
@@ -6,8 +6,60 @@ import FeelSpecial from "../components/Feelspecial/feelspecial";
 import WindowPeak from "../components/peekWindow/peekwindow";
 import MobileScroll from "../components/MobileScroll/mobilescroll";
 import Footer from "../components/footer/footer";
-
+import PaymentModal from "./PaymentModal";
+import { io } from "socket.io-client";
+import { useSocketRef } from "./contextProvider/SocketProvider";
 const HomePage = () => {
+  const [isPaymentModalOpen, setIsPaymentModalOpen] = useState(false);
+  const [amount,setAmount]=useState(0);
+  const socket = useSocketRef();
+
+  console.log(socket.current);
+  const handleConfirmPayment = () => {
+    // Trigger a socket event for payment confirmation
+    socket.current.emit("paymentAccepted", { status: "confirmed" });
+    setIsPaymentModalOpen(false)
+  };
+
+  // Function to handle payment decline
+  const handleDeclinePayment = () => {
+    // Trigger a socket event for payment decline
+    socket.current.emit("paymentDeclined", { status: "declined" });
+    setIsPaymentModalOpen(false)
+  };
+
+  useEffect(() => {
+    // Listen for paymentConfirmation events from the server
+    socket.current = io("http://localhost:4000").connect();
+    if(socket.current){
+      console.log("connected to socket");
+    }
+    // console.log(socket.current.connected);
+    if (sessionStorage.getItem("mobileNumber")) {
+      const mobileNumber = sessionStorage.getItem("mobileNumber");
+      socket.current.emit("joinPool", mobileNumber);
+    }
+    // if(socket.current){
+    // console.log(socket.current.connected)
+    socket.current.on("paymentStatus", (event) => {
+      try {
+        console.log("payment status received", event.status);
+        setAmount(event.Amount)
+        setIsPaymentModalOpen(true);
+      } catch (error) {
+        console.error("Error handling paymentStatus event:", error);
+      }
+    });
+    // }
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      socket.current.disconnect();
+      console.log("Disconnected from socket");
+      socket.current.off("paymentStatus");
+    };
+  }, [socket]);
+
   return (
     <>
       <Header />
@@ -17,6 +69,17 @@ const HomePage = () => {
       <FeelSpecial />
       <MobileScroll />
       <Footer />
+
+      {/* Render the PaymentModal component conditionally */}
+      {isPaymentModalOpen && (
+        <PaymentModal
+          onClose={() => setIsPaymentModalOpen(false)}
+          onConfirmPayment={handleConfirmPayment}
+          onDeclinePayment={handleDeclinePayment}
+          Amount={amount}
+          // Add other props as needed
+        />
+      )}
     </>
   );
 };
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,24 +1,60 @@
-
 import Button from "../components/common/Button";
-import React from "react";
+import React, { useState,useRef,useEffect } from "react";
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { useSocketRef } from "./contextProvider/SocketProvider";
 
 const Login = () => {
   const auth = getAuth();
   const googleProvider = new GoogleAuthProvider();
+  const [showMobileInput, setShowMobileInput] = useState(false);
+  const [mobileNumber, setMobileNumber] = useState("");
+  const socketRef = useSocketRef();
 
   const handleGoogleSignIn = async () => {
     try {
       await signInWithPopup(auth, googleProvider);
+      // After successful Google Sign-In, show the mobile number input
+      setShowMobileInput(true);
     } catch (error) {
       console.error("Error signing in with Google:", error);
     }
   };
 
+  const handleMobileNumberChange = (e) => {
+    setMobileNumber(e.target.value);
+  };
+
+  const handleMobileNumberSubmit = () => {
+    console.log("Mobile Number:", mobileNumber);
+    sessionStorage.setItem("mobileNumber", mobileNumber);
+    if (socketRef.current) {
+      const mobileNumber = sessionStorage.getItem("mobileNumber");
+      socketRef.current.emit("joinPool", mobileNumber);
+    }
+    setShowMobileInput(false);
+
+  };
+
   return (
     <div>
-      <Button onClick={handleGoogleSignIn} buttonText="Start Pooling"/>
-      {/* Add other authentication methods/buttons here */}
+      <Button onClick={handleGoogleSignIn} buttonText="Start Pooling" />
+      {showMobileInput && (
+        <div className="bg-white p-4 ">
+          <input
+            type="tel"
+            placeholder="Enter your mobile number"
+            value={mobileNumber}
+            onChange={handleMobileNumberChange}
+            className=" text-white rounded-lg border-2 border-gray-300 p-2"
+          />
+          <button
+            onClick={handleMobileNumberSubmit}
+            className="bg-black text-white rounded-lg px-4 py-2 mt-2"
+          >
+            Confirm Number
+          </button>
+        </div>
+      )}
     </div>
   );
 };
