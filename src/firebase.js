a0154510943a01befb7dfd1e1eb39162c93f27ca
SRIRAM ADARI <lakshmisriramadari1427@gmail.com>
2023-10-01 12:16:19 +0530

    firebase config changed

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,24 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyB1vIsg3SZOYkKSt9teBve1RFQsdn9GwJc",
+//   authDomain: "poolpay-c3abf.firebaseapp.com",
+//   projectId: "poolpay-c3abf",
+//   storageBucket: "poolpay-c3abf.appspot.com",
+//   messagingSenderId: "346592004812",
+//   appId: "1:346592004812:web:cb8fc9333a598aee105069",
+// };
+
+//sriramAdari
 const firebaseConfig = {
-  apiKey: "AIzaSyB1vIsg3SZOYkKSt9teBve1RFQsdn9GwJc",
-  authDomain: "poolpay-c3abf.firebaseapp.com",
-  projectId: "poolpay-c3abf",
-  storageBucket: "poolpay-c3abf.appspot.com",
-  messagingSenderId: "346592004812",
-  appId: "1:346592004812:web:cb8fc9333a598aee105069",
+  apiKey: "AIzaSyBBXL2MdwDKzovpdyrb7ts9JDXwamHxO_I",
+  authDomain: "poolpay-9748c.firebaseapp.com",
+  projectId: "poolpay-9748c",
+  storageBucket: "poolpay-9748c.appspot.com",
+  messagingSenderId: "52589605705",
+  appId: "1:52589605705:web:4c53cb866cd7df891de098",
+  measurementId: "G-KTCP9653YM"
 };
 
 // Initialize Firebase
